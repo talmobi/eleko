ea797d4ed84707848b3ff13fdbe06a4ea36f1435
Christian Talmo <talmo.christian@gmail.com>
2018-12-10 14:00:26 +0200

    fix path

--- main.js
+++ main.js
@@ -258,7 +258,9 @@ function createWindow ()
   } )
 
   // and load the index.html of the app
-  // mainWindow.loadFile( 'index.html' )
+  // mainWindow.loadFile(
+  // path.join( __dirname, 'index.html' )
+  // )
 
   // example load url
   console.log( 'playing video id: ' + _videoId )
