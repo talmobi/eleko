dddd304bfd6c6ebc812fd97926086a0e20b865d3
Christian Talmo <talmo.christian@gmail.com>
2020-02-05 21:30:01 +0200

    refactor: move a helper fn's to elekid.js as their own api

---
+++ elekid.js
@@ -0,0 +1,264 @@
+const electron = require( 'electron' )
+
+const fs = require( 'fs' )
+const path = require( 'path' )
+const url = require( 'url' )
+
+const eeto = require( 'eeto' )
+
+// Module to control application life
+const app = electron.app
+
+// Module to create native browser window
+const BrowserWindow = electron.BrowserWindow
+
+const api = eeto()
+
+module.exports = api
+api.launch = launch
+api.goto = goto
+api.waitFor = waitFor
+api.evaluate = evaluate
+api.onBeforeRequest = onBeforeRequest
+
+const WAITFOR_TIMEOUT_TIME = 1000 * 30 // 30 seconds
+const POLL_INTERVAL = 200 // milliseconds
+const LAUNCH_TIMEOUT_TIME = 10 * 1000
+
+// TODO add software rendering opts
+// ref: https://swiftshader.googlesource.com/SwiftShader
+// ref: https://github.com/google/swiftshader
+const DEFAULT_OPTS = {
+  show: false,
+  width: 800,
+  height: 600,
+  webPreferences: {
+    autoplayPolicy: [ 'no-user-gesture-required', 'user-gesture-required', 'document-user-activation-required' ][ 2 ],
+
+    // javascript: false,
+    images: false,
+    webgl: false,
+
+    nodeIntegration: false,
+    webviewTag: false,
+    contextIsolation: true,
+    enableRemoteModule: false,
+
+    // preload: path.join( __dirname, 'preload.js' )
+  }
+}
+
+function launch ( opts )
+{
+  return new Promise( function ( resolve, reject ) {
+    let done = false
+    const _timeout = setTimeout( function () {
+      if ( _done ) return
+      _done = true
+      reject( 'error: timed out launch' )
+    }, LAUNCH_TIMEOUT_TIME )
+
+    // Create the browser window
+    const mainWindow = new BrowserWindow( {
+      show: false,
+      width: 800,
+      height: 600,
+      webPreferences: {
+        autoplayPolicy: [ 'no-user-gesture-required', 'user-gesture-required', 'document-user-activation-required' ][ 2 ],
+
+        // javascript: false,
+        images: false,
+        webgl: false,
+
+        nodeIntegration: false,
+        webviewTag: false,
+        contextIsolation: true,
+        enableRemoteModule: false,
+        preload: path.join( __dirname, 'preload.js' )
+      }
+    } )
+
+    mainWindow.once( 'ready-to-show', function () {
+      if ( _done ) return
+      _done = true
+      resolve( mainWindow )
+    } )
+  } )
+}
+
+function setUserAgent ( mainWindow )
+{
+  const session = mainWindow.webContents.session
+
+  // set user-agent lowest compatible
+  session.setUserAgent( 'Mozilla/5.0 (https://github.com/talmobi/elekid)' )
+}
+
+// mainWindow[, options], query[, ...args])
+function waitFor ( mainWindow, query, ...args )
+{
+  let opts = {}
+  if ( typeof query === 'object' ) {
+    opts = query
+    query = args[ 0 ]
+    args = args.slice( 1 )
+  }
+
+  if ( typeof query === 'object' ) {
+    throw new Error(
+      'query must be a string, number or function\n' +
+      'function waitFor( mainWindow[, options], query[, ...args] )'
+    )
+  }
+
+  return new Promise ( function ( resolve, reject ) {
+    if ( typeof query === 'number' ) {
+      return setTimeout( resolve, query )
+    }
+
+    let fnString
+    if ( typeof query === 'string' ) {
+      fnString = (`
+        ;(function () {
+          console.log( ' === waitFor === ' )
+          return !!document.querySelector( '${ query }' )
+        })()
+      `)
+    } else {
+      if ( typeof query !== 'function' ) {
+        throw new Error(
+          'query must be a string, number or function\n' +
+          'function waitFor( mainWindow[, options], query[, ...args] )'
+        )
+      }
+      fnString = parseFunction( query, args )
+    }
+
+    const startTime = Date.now()
+
+    function callback ( result ) {
+      console.log( ' === callback === ' )
+
+      if ( result ) {
+        return resolve()
+      }
+
+      const ms = Number( opts.polling ) || POLL_INTERVAL
+
+      setTimeout( next, ms )
+    }
+
+    next()
+
+    async function next () {
+      console.log( ' === next === ' )
+
+      const now = Date.now()
+      const delta = ( now - startTime )
+      if ( delta > WAITFOR_TIMEOUT_TIME ) {
+        return reject( 'error: timed out waitFor' )
+      }
+
+      try {
+        const p = await mainWindow.webContents.executeJavaScript(
+          fnString,
+          true
+        )
+        callback( p )
+      } catch ( err ) {
+        throw 'error: waitFor query: ' + query
+      }
+    }
+  } )
+}
+
+function goto ( mainWindow, url )
+{
+  console.log( ' === goto === ' )
+
+  return new Promise( async function ( resolve, reject ) {
+    try {
+      const p = await mainWindow.loadURL( url )
+      console.log( ' >> GOTO DONE << ' )
+      resolve()
+    } catch ( err ) {
+      reject( err )
+    }
+  } )
+}
+
+function evaluate ( mainWindow, fn, ...args )
+{
+  console.log( ' === evaluate === ' )
+
+  const fnString = parseFunction( fn, args )
+
+  return new Promise( async function ( resolve, reject ) {
+    const p = await mainWindow.webContents.executeJavaScript(
+      fnString,
+      true
+    )
+    resolve( p )
+  } )
+}
+
+function onBeforeRequest ( mainWindow, filter )
+{
+  const session = mainWindow.webContents.session
+
+  // cancel or do something before requests
+  session.webRequest.onBeforeRequest(
+    function ( details, callback ) {
+      const shouldBlock = filter( details )
+      if ( shouldBlock ) {
+        // block
+        console.log( ' (x) url blocked: ' + url.slice( 0, 23 ) )
+        return callback( { cancel: true } )
+      }
+    }
+  )
+}
+
+// load adblock plus easylist to block urls related to ads
+let easyList
+try {
+  easyList = fs.readFileSync(
+    path.join( __dirname, './easylist.txt' ), 'utf8'
+  ).split( /\r?\n\r?/ )
+} catch ( err ) {
+  console.log( 'failed to load easylist.txt ( try downloading with "npm run download-easylist" )' )
+  easyList = []
+}
+console.log( 'easyList length: ' + easyList.length )
+
+// called in session.webRequest.onBeforeRequest(...) to allow/reject ads
+function containsAds ( url )
+{
+  if ( !easyList ) return false
+
+  url = String( url )
+
+  for ( let i = 0; i < easyList.length; i++ ) {
+    const item = easyList[ i ] || ''
+    if ( item.length > 3 && url.indexOf( item ) >= 0 ) {
+      return true
+    }
+  }
+
+  return false
+}
+
+function parseFunction ( fn, args )
+{
+  const fnString = fn.toString()
+
+  args = args.map( function ( arg ) { return JSON.stringify( arg ) } )
+
+  const wrapped = (`
+    ;(${ fnString })(${ args.join( ',' ) });
+  `)
+
+  console.log( wrapped )
+
+  return wrapped
+}
--- main.js
+++ main.js
@@ -4,20 +4,7 @@ const fs = require( 'fs' )
 const path = require( 'path' )
 const url = require( 'url' )
 
-function parseFunction ( fn, args )
-{
-  const fnString = fn.toString()
-
-  args = args.map( function ( arg ) { return JSON.stringify( arg ) } )
-
-  const wrapped = (`
-    ;(${ fnString })(${ args.join( ',' ) });
-  `)
-
-  console.log( wrapped )
-
-  return wrapped
-}
+const ek = require( './elekid.js' )
 
 process.on( 'uncaughtException', function ( error ) {
   console.log( ' === uncaughtException === ' )
@@ -34,35 +21,6 @@ process.on( 'uncaughtException', function ( error ) {
   process.exit( 1 )
 } )
 
-// load adblock plus easylist to block urls related to ads
-let easyList
-try {
-  easyList = fs.readFileSync(
-    path.join( __dirname, './easylist.txt' ), 'utf8'
-  ).split( /\r?\n\r?/ )
-} catch ( err ) {
-  console.log( 'failed to load easylist.txt ( try downloading with "npm run download-easylist" )' )
-  easyList = []
-}
-console.log( 'easyList length: ' + easyList.length )
-
-// called in session.webRequest.onBeforeRequest(...) to allow/reject ads
-function containsAds ( url )
-{
-  if ( !easyList ) return false
-
-  url = String( url )
-
-  for ( let i = 0; i < easyList.length; i++ ) {
-    const item = easyList[ i ] || ''
-    if ( item.length > 3 && url.indexOf( item ) >= 0 ) {
-      return true
-    }
-  }
-
-  return false
-}
-
 // Module to control application life
 const app = electron.app
 
@@ -74,204 +32,18 @@ const BrowserWindow = electron.BrowserWindow
 // is garbage collected
 let mainWindow
 
-// Setup IPC ( inter process communication ) between the electron application and your nodejs code
-const ipcMain = require( 'electron' ).ipcMain
-
-ipcMain.on( 'set-size', function ( evt, data ) {
-  console.log( '[ipcMain]: set-size' )
-  console.log( data )
-
-  if ( mainWindow ) {
-    mainWindow.setSize( data.width, data.height, 0 )
-    // mainWindow.show()
-  }
-} )
-
-ipcMain.on( 'ready-to-show', function ( evt, data ) {
-  console.log( '[ipcMain]: ready-to-show' )
-  console.log( data )
-
-  if ( mainWindow ) {
-    // mainWindow.setSize( data.width, data.height, 0 )
-    // mainWindow.show()
-  }
-} )
-
-ipcMain.on( 'tick', function ( evt, data ) {
-  console.log( '[ipcMain]: tick: ' + data )
-} )
-
-ipcMain.on( 'show', function ( evt, data ) {
-  console.log( '[ipcMain]: show' )
-  console.log( data )
-
-  if ( mainWindow ) {
-    mainWindow.show()
-  }
-} )
-
-ipcMain.on( 'hide', function ( evt, data ) {
-  console.log( '[ipcMain]: hide' )
-  console.log( data )
-
-  if ( mainWindow ) {
-    mainWindow.hide()
-  }
-} )
-
-ipcMain.on( 'quit', function ( evt, data ) {
-  console.log( '[ipcMain]: quit' )
-  console.log( data )
-
-  app.quit()
-} )
-
 // example data
 const _philipGlassHoursVideoId = 'Wkof3nPK--Y'
 const pkmnBlueWaveId = 'pFbkURxNKPE' // requires h264
 const creedenceId = 'Gu2pVPWGYMQ' // AV1
 const _urlTemplate = 'https://www.youtube.com/watch/$videoId'
-const _videoId = process.argv.slice( 2 )[ 0 ] || _philipGlassHoursVideoId
-
-function execFunc ( fn )
-{
-  const fnString = fn.toString()
-  return ( '(' + fnString + ')();' )
-}
-
-const MAX_TIMEOUT = 1000 * 30 // 30 seconds
-const POLL_INTERVAL = 200 // milliseconds
-
-// mainWindow[, options], query[, ...args])
-function waitFor ( mainWindow, query, ...args )
-{
-  let opts = {}
-  if ( typeof query === 'object' ) {
-    opts = query
-    query = args[ 0 ]
-    args = args.slice( 1 )
-  }
-
-  if ( typeof query === 'object' ) {
-    throw new Error(`
-query must be a string, number or function
-function waitFor( mainWindow[, options], query[, ...args] )
-      `)
-  }
-
-  return new Promise ( function ( resolve, reject ) {
-    if ( typeof query === 'number' ) {
-      return setTimeout( resolve, query )
-    }
-
-    let fnString
-    if ( typeof query === 'string' ) {
-      fnString = (`
-        ;(function () {
-          console.log( ' === waitFor === ' )
-          return !!document.querySelector( '${ query }' )
-        })()
-      `)
-    } else {
-      if ( typeof query !== 'function' ) {
-        throw new Error(`
-query must be a string, number or function
-function waitFor( mainWindow[, options], query[, ...args] )
-      `)
-      }
-      fnString = parseFunction( query, args )
-    }
-
-    const startTime = Date.now()
-
-    function callback ( result ) {
-      console.log( ' === callback === ' )
-
-      if ( result ) {
-        return resolve()
-      }
-
-      const ms = Number( opts.polling ) || POLL_INTERVAL
-
-      setTimeout( next, ms )
-    }
-
-    next()
-
-    async function next () {
-      console.log( ' === next === ' )
-
-      const now = Date.now()
-      const delta = ( now - startTime )
-      if ( delta > MAX_TIMEOUT ) {
-        return reject( 'timed out' )
-      }
-
-      try {
-        const p = await mainWindow.webContents.executeJavaScript(
-          fnString,
-          true
-        )
-        callback( p )
-      } catch ( err ) {
-        throw 'error: waitFor query: ' + query
-      }
-    }
-  } )
-}
-
-function goto ( mainWindow, url )
-{
-  console.log( ' === goto === ' )
-
-  return new Promise( async function ( resolve, reject ) {
-    try {
-      const p = await mainWindow.loadURL( url )
-      console.log( ' >> GOTO DONE << ' )
-      resolve()
-    } catch ( err ) {
-      reject( err )
-    }
-  } )
-}
-
-function evaluate ( mainWindow, fn, ...args )
-{
-  console.log( ' === evaluate === ' )
-
-  const fnString = parseFunction( fn, args )
-
-  return new Promise( async function ( resolve, reject ) {
-    const p = await mainWindow.webContents.executeJavaScript(
-      fnString,
-      true
-    )
-    resolve( p )
-  } )
-}
-
-function onBeforeRequest ( mainWindow, filter )
-{
-  const session = mainWindow.webContents.session
-
-  // cancel or do something before requests
-  session.webRequest.onBeforeRequest(
-    function ( details, callback ) {
-      const shouldBlock = filter( details )
-      if ( shouldBlock ) {
-        // block
-        console.log( ' (x) url blocked: ' + url.slice( 0, 23 ) )
-        return callback( { cancel: true } )
-      }
-    }
-  )
-}
+const _videoId = creedenceId
 
 async function createWindow ()
 {
   // Create the browser window
   mainWindow = new BrowserWindow( {
-    show: true,
+    show: false,
     width: 800,
     height: 600,
     webPreferences: {
@@ -333,12 +105,9 @@ async function createWindow ()
     }
   )
 
-  // mainWindow.webContents.executeJavaScript( 'alert("hello!")')
-  // mainWindow.webContents.executeJavaScript( execFunc( 'playVideo' ) )
-
   mainWindow.on( 'ready-to-show', function () {
     console.log( 'ready-to-show' )
-    mainWindow.show()
+    // mainWindow.show()
   } )
 
   // and load the index.html of the app
