87d595712ae12c27460ae07b5eaa56a3a12eb45c
Christian Talmo <talmo.christian@gmail.com>
2020-03-06 12:59:01 +0200

    test: remove deprecated page.call fn

--- test/test.js
+++ test/test.js
@@ -37,7 +37,7 @@ let _browser
 let _page
 test( 'launch', async function ( t ) {
   t.timeoutAfter( 8000 )
-  t.plan( 3 )
+  t.plan( 2 )
 
   const browser = await eleko.launch()
   t.ok( browser.spawn.pid )
@@ -68,7 +68,6 @@ test( 'launch', async function ( t ) {
   await page.setUserAgent( userAgent )
   console.log( 'setUserAgent' )
 
-  t.equal( userAgent, await page.call( 'webContents.session.getUserAgent' ), 'user agent set OK' )
   t.equal( userAgent, await page.getUserAgent(), 'user agent set OK' )
 } )
 
