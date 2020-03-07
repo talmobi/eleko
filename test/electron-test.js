906ffb458c3834aac22aa74ba18ee5ddc88609ec
Christian Talmo <talmo.christian@gmail.com>
2020-03-07 17:58:31 +0200

    refactor: move tests outside of async context so they are noticed by tape feat: use eleko.newPage wrapper instead of removed eleko.goto

--- test/electron-test.js
+++ test/electron-test.js
@@ -59,15 +59,14 @@ app.dock && app.dock.hide && app.dock.hide()
 // Module to create native browser window
 const BrowserWindow = electron.BrowserWindow
 
-// Keep a global ref of the window object, if you don't, the widow
-// will be closed automatically when the JavaScript object
-// is garbage collected
-let mainWindow
+let page, session
+
+const userAgent = 'Mozilla/5.0 (https://github.com/talmobi/eleko)'
 
 async function createWindow ()
 {
   // Create the browser window
-  mainWindow = new BrowserWindow( {
+  page = await eleko.newPage( {
     show: false,
     width: 800,
     height: 600,
@@ -86,173 +85,172 @@ async function createWindow ()
     }
   } )
 
-  const session = mainWindow.webContents.session
+  session = page.win.webContents.session
 
-  // set user-agent lowest compatible
-  // set user-agent lowest compatible
-  const userAgent = 'Mozilla/5.0 (https://github.com/talmobi/eleko)'
-  session.setUserAgent( 'Mozilla/5.0 (https://github.com/talmobi/eleko)' )
-  test( 'goto local index.html', async function ( t ) {
-    t.plan( 1 )
-    t.equal( userAgent, await session.getUserAgent(), 'user agent set OK' )
-  } )
-
-
-  test( 'goto local index.html', async function ( t ) {
-    t.plan( 1 )
-
-    const port = server.address().port
-    const url = `http://127.0.0.1:${ port }/index.html`
-    console.log( url )
-
-    await eleko.goto( mainWindow, url )
-
-    const title = await eleko.evaluate( mainWindow, function () {
-      return document.title
-    } )
-
-    t.equal( title, 'Adorable baby giraffes' )
-  } )
-
-  test( 'evaluate title with string args', async function ( t ) {
-    t.plan( 1 )
-
-    await eleko.evaluate( mainWindow, function ( newTitle, selector ) {
-      document[ selector ] = newTitle
-    }, 'giraffe-title', 'title' )
-
-    const title = await eleko.evaluate( mainWindow, function () {
-      return document.title
-    } )
-
-    t.equal( title, 'giraffe-title' )
-  } )
-
-  test( 'evaluate title with object args', async function ( t ) {
-    t.plan( 1 )
-
-    await eleko.evaluate( mainWindow, function ( data ) {
-      document[ data.selector ] = data.title
-    }, { title: 'whale-title', selector: 'title' } )
-
-    const title = await eleko.evaluate( mainWindow, function () {
-      return document.title
-    } )
-
-    t.equal( title, 'whale-title' )
-  } )
-
-  test( 'waitFor string', async function ( t ) {
-    t.plan( 3 )
-
-    const now = Date.now()
-    const text = await eleko.evaluate( mainWindow, function ( data ) {
-      const el = document.querySelector( 'div[type=monkey]' )
-      return el && el.textContent
-    } )
-
-    await eleko.evaluate( mainWindow, function ( data ) {
-      triggerWaitFor( 'monkey' )
-    } )
-
-    await eleko.waitFor( mainWindow, 'div[type=monkey]' )
-
-    // time waited
-    const delta = Date.now() - now
-
-    const newText = await eleko.evaluate( mainWindow, function ( data ) {
-      const el = document.querySelector( 'div[type=monkey]' )
-      return el && el.textContent
-    } )
-
-    t.ok( text === null || text === undefined, 'element did not exists yet OK' )
-    t.equal( newText, 'monkey text', 'element added later during waitFor OK' )
-    t.ok( delta >= 1000 && delta < 2000, 'timed waited reasonable' )
-  } )
-
-  test( 'waitFor function', async function ( t ) {
-    t.plan( 3 )
-
-    const now = Date.now()
-    const text = await eleko.evaluate( mainWindow, function ( data ) {
-      const el = document.querySelector( 'div[type=whale]' )
-      return el && el.textContent
-    } )
-
-    await eleko.evaluate( mainWindow, function ( data ) {
-      triggerWaitFor( 'whale' )
-    } )
-
-    await eleko.waitFor( mainWindow, function () {
-      const el = document.querySelector( 'div[type=whale]' )
-      return !!el
-    } )
-
-    // time waited
-    const delta = Date.now() - now
-
-    const newText = await eleko.evaluate( mainWindow, function ( data ) {
-      const el = document.querySelector( 'div[type=whale]' )
-      return el && el.textContent
-    } )
-
-    t.ok( text === null || text === undefined, 'element did not exists yet OK' )
-    t.equal( newText, 'whale text', 'element added later during waitFor OK' )
-    t.ok( delta >= 1000 && delta < 2000, 'timed waited reasonable' )
-  } )
-
-  test( 'evaluate promise', async function ( t ) {
-    t.plan( 2 )
-
-    await eleko.evaluate( mainWindow, function ( newTitle, selector ) {
-      document[ selector ] = newTitle
-    }, 'before-promise-title', 'title' )
-
-    const title = await eleko.evaluate( mainWindow, function () {
-      return document.title
-    } )
-
-    await eleko.evaluate( mainWindow, function ( newTitle, selector ) {
-      setTimeout( function () {
-        document[ selector ] = newTitle
-      }, 1000 )
-    }, 'after-promise-title', 'title' )
-
-    const newTitle = await eleko.evaluate( mainWindow, function () {
-      return new Promise( function ( resolve, reject ) {
-        setTimeout( function () {
-          resolve( document.title )
-        }, 1000 )
-      } )
-    } )
-
-    t.equal( title, 'before-promise-title' )
-    t.equal( newTitle, 'after-promise-title' )
-  } )
-
-  test( 'close test http server', function ( t ) {
-    t.plan( 1 )
-
-    server.close( function () {
-      t.pass( 'server closed' )
-    } )
-  } )
-
-  test.onFinish( function () {
-    app.quit()
-  } )
+  session.setUserAgent( userAgent )
 
   // Open the DevTools
-  // mainWindow.webContents.openDevTools()
+  // page.win.webContents.openDevTools()
 
-  // Emitted when the window is closed
-  mainWindow.on( 'closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element
-    mainWindow = null
-  } )
+  return page
 }
+
+test( 'create window', async function ( t ) {
+  t.plan( 1 )
+  const p = await createWindow()
+  t.equal( p, page )
+} )
+
+test( 'goto local index.html', async function ( t ) {
+  t.plan( 1 )
+  t.equal( userAgent, await session.getUserAgent(), 'user agent set OK' )
+} )
+
+test( 'goto local index.html', async function ( t ) {
+  t.plan( 1 )
+
+  const port = server.address().port
+  const url = `http://127.0.0.1:${ port }/index.html`
+  console.log( url )
+
+  console.log( 'going goto' )
+  await page.goto( url )
+  console.log( 'done goto' )
+
+  const title = await eleko.evaluate( page.win, function () {
+    return document.title
+  } )
+
+  t.equal( title, 'Adorable baby giraffes' )
+} )
+
+test( 'evaluate title with string args', async function ( t ) {
+  t.plan( 1 )
+
+  await eleko.evaluate( page.win, function ( newTitle, selector ) {
+    document[ selector ] = newTitle
+  }, 'giraffe-title', 'title' )
+
+  const title = await eleko.evaluate( page.win, function () {
+    return document.title
+  } )
+
+  t.equal( title, 'giraffe-title' )
+} )
+
+test( 'evaluate title with object args', async function ( t ) {
+  t.plan( 1 )
+
+  await eleko.evaluate( page.win, function ( data ) {
+    document[ data.selector ] = data.title
+  }, { title: 'whale-title', selector: 'title' } )
+
+  const title = await eleko.evaluate( page.win, function () {
+    return document.title
+  } )
+
+  t.equal( title, 'whale-title' )
+} )
+
+test( 'waitFor string', async function ( t ) {
+  t.plan( 3 )
+
+  const now = Date.now()
+  const text = await eleko.evaluate( page.win, function ( data ) {
+    const el = document.querySelector( 'div[type=monkey]' )
+    return el && el.textContent
+  } )
+
+  await eleko.evaluate( page.win, function ( data ) {
+    triggerWaitFor( 'monkey' )
+  } )
+
+  await eleko.waitFor( page.win, 'div[type=monkey]' )
+
+  // time waited
+  const delta = Date.now() - now
+
+  const newText = await eleko.evaluate( page.win, function ( data ) {
+    const el = document.querySelector( 'div[type=monkey]' )
+    return el && el.textContent
+  } )
+
+  t.ok( text === null || text === undefined, 'element did not exists yet OK' )
+  t.equal( newText, 'monkey text', 'element added later during waitFor OK' )
+  t.ok( delta >= 1000 && delta < 2000, 'timed waited reasonable' )
+} )
+
+test( 'waitFor function', async function ( t ) {
+  t.plan( 3 )
+
+  const now = Date.now()
+  const text = await eleko.evaluate( page.win, function ( data ) {
+    const el = document.querySelector( 'div[type=whale]' )
+    return el && el.textContent
+  } )
+
+  await eleko.evaluate( page.win, function ( data ) {
+    triggerWaitFor( 'whale' )
+  } )
+
+  await eleko.waitFor( page.win, function () {
+    const el = document.querySelector( 'div[type=whale]' )
+    return !!el
+  } )
+
+  // time waited
+  const delta = Date.now() - now
+
+  const newText = await eleko.evaluate( page.win, function ( data ) {
+    const el = document.querySelector( 'div[type=whale]' )
+    return el && el.textContent
+  } )
+
+  t.ok( text === null || text === undefined, 'element did not exists yet OK' )
+  t.equal( newText, 'whale text', 'element added later during waitFor OK' )
+  t.ok( delta >= 1000 && delta < 2000, 'timed waited reasonable' )
+} )
+
+test( 'evaluate promise', async function ( t ) {
+  t.plan( 2 )
+
+  await eleko.evaluate( page.win, function ( newTitle, selector ) {
+    document[ selector ] = newTitle
+  }, 'before-promise-title', 'title' )
+
+  const title = await eleko.evaluate( page.win, function () {
+    return document.title
+  } )
+
+  await eleko.evaluate( page.win, function ( newTitle, selector ) {
+    setTimeout( function () {
+      document[ selector ] = newTitle
+    }, 1000 )
+  }, 'after-promise-title', 'title' )
+
+  const newTitle = await eleko.evaluate( page.win, function () {
+    return new Promise( function ( resolve, reject ) {
+      setTimeout( function () {
+        resolve( document.title )
+      }, 1000 )
+    } )
+  } )
+
+  t.equal( title, 'before-promise-title' )
+  t.equal( newTitle, 'after-promise-title' )
+} )
+
+test( 'close test http server', function ( t ) {
+  t.plan( 1 )
+
+  server.close( function () {
+    t.pass( 'server closed' )
+  } )
+} )
+
+test.onFinish( function () {
+  app.quit()
+} )
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -271,12 +269,3 @@ app.on( 'window-all-closed', function () {
 
   app.quit()
 })
-
-app.on( 'activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if ( mainWindow === null ) {
-    createWindow()
-  }
-})
-
