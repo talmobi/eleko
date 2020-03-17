5085d594971206bd1ba3563c511b6973fc464ac4
Christian Talmo <talmo.christian@gmail.com>
2020-03-17 17:00:10 +0200

    feat: add new listen api to await until connected to a ipc socket

--- stdio-ipc.js
+++ stdio-ipc.js
@@ -5,6 +5,7 @@ const rimraf = require( 'rimraf' )
 const eeto = require( 'eeto' )
 
 module.exports.create = create
+module.exports.listen = listen
 
 const _ipcPaths = []
 function clean () {
@@ -31,6 +32,127 @@ function getIPCPath ( name ) {
   rimraf.sync( ipcPath )
 
   return ipcPath
+}
+
+function listen ( name ) {
+  return new Promise ( function ( resolve, reject ) {
+    const ipcPath = getIPCPath( name )
+    console.log( 'listen:name:' + name )
+
+    const api = eeto()
+
+    connect()
+    function connect () {
+      setTimeout( function () {
+        const socket = net.connect( ipcPath )
+        socket.once( 'connect', function () {
+          console.log( 'listen:connected' )
+          api.socket = socket
+          api.emit( 'connected' )
+          resolve( api )
+        } )
+        socket.once( 'error', function () {
+          connect()
+        } )
+      }, 250 )
+    }
+
+    api.buffer = ''
+
+    api.ids = 1
+    api.promises = []
+
+    api._ready = false
+    api._messages = []
+
+    function attach () {
+      api.socket.on( 'data', function api_onData ( chunk ) {
+        onData( api, chunk )
+      } )
+    }
+
+    api.parseMessage = function api_parseMessage ( jsonString ) {
+      const json = JSON.parse( jsonString )
+      api._messages.push( json )
+      drain()
+    }
+
+    api.send = function api_send ( json ) {
+      return send( api, json )
+    }
+
+    api.promise = function api_promise ( json ) {
+      return promise( api, json )
+    }
+
+    api.once( 'ready', function () {
+      api._ready = true
+      drain()
+    } )
+
+    api.once( 'connected', function () {
+      attach()
+      drain()
+    } )
+
+    function drain () {
+      if ( !api._ready ) return
+
+      const messages = api._messages
+      api._messages = []
+      for ( let i = 0; i < messages.length; i++ ) {
+        const evt = messages[ i ]
+
+        switch ( evt.type ) {
+          case 'promise':
+            {
+              function callback ( err, data ) {
+                if ( callback.done ) return
+                callback.done = true
+                api.send( {
+                  type: 'promise:response',
+                  id: evt.id,
+                  error: err,
+                  content: data
+                } )
+              }
+
+              const p = {
+                resolve: function ( data ) {
+                  callback( undefined, data )
+                },
+                reject: function ( err ) {
+                  callback( err )
+                },
+                data: evt.content
+              }
+
+              api.emit( 'promise', p )
+            }
+            break
+
+          case 'promise:response':
+            {
+              const p = api.promises[ evt.id ]
+              delete api.promises[ evt.id ]
+              if ( evt.error ) {
+                p.reject( evt.error )
+              } else {
+                // console.log( evt )
+                // console.log( 'promise content: ' + evt.content )
+                p.resolve( evt.content )
+              }
+            }
+            break
+
+          default:
+            api.emit( evt.type, evt.content )
+        }
+      }
+    }
+
+    return api
+  } )
 }
 
 
